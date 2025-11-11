8de29cf920a039b0a4775e36700fd6d1fdc03393
x0djers <x0djers@gmail.com>
2025-11-11 18:49:15 +0300

    chore: make the Alembic setup

---
+++ migrations/env.py
@@ -0,0 +1,43 @@
+from alembic import context
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+
+from src.core.db.base import Base
+from src.core.config import settings
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def run_migrations_offline() -> None:
+    url = settings.DB.get_sync_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online() -> None:
+    connectable = engine_from_config(
+        {"sqlalchemy.url": settings.DB.get_sync_url()},
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
