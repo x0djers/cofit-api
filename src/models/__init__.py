2705db8eeddf3efcd674e732fc8b8e796e4d089b
x0djers <x0djers@gmail.com>
2025-12-02 18:02:58 +0300

    fix: remove an erroneous model import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,12 +2,10 @@ from .user import User
 from .diary import Diary
 from .training import Training
 from .exercise import Exercise
-from .training_exercise import TrainingExercise
 
 __all__ = [
     "User",
     "Diary",
     "Training",
     "Exercise",
-    "TrainingExercise",
 ]
